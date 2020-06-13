40bc976a638b7c6a2d1f0d8a91571b96544c9a8d
KelwinPF <kelwinpferreira@gmail.com>
2020-06-13 11:24:43 -0300

    variaveis de ambiente

--- src/main/java/com/digitalinnovationone/springboot/AppController.java
+++ src/main/java/com/digitalinnovationone/springboot/AppController.java
@@ -8,11 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @Value("${app.message}")
     private String appMessage;
-
+    
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
+    
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
     }
-
-
+    
+    @GetMapping("/envVariable")
+    public String getEnvironmentVariable() {
+    	return "a seguinte varivel foi passada" + dbEnvironmentVariable;
+    }
 }
